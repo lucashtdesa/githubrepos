888dd837e8cc24ef3367decd304ee2af37b0cbad
Lucas Henrique <68077602+lucashtdesa@users.noreply.github.com>
2022-03-24 18:00:10 -0300

    Projeto completo
    O projeto já existia em minha outra máquina, não havia ainda deixado no GitHub

---
+++ GitHubRepositories/Controllers/RepositoryController.cs
@@ -0,0 +1,47 @@
+﻿using GitHubRepositories.Model;
+using GitHubRepositories.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GitHubRepositories.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RepositoryController : ControllerBase
+    {
+        private readonly IRepositoryService _repositoryService;
+
+        public RepositoryController(IRepositoryService repositoryService)
+        {
+            _repositoryService = repositoryService;
+        }
+
+        [HttpGet("/repos/{name}")]
+        public async Task<ActionResult<List<Repository>>> GetRepositories(string name)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var repos = await _repositoryService.FindByNameAsync(name);
+                if (repos == null)
+                {
+                    return NotFound();
+                }
+                return Ok(repos);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ GitHubRepositories/Model/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GitHubRepositories.Model
+{
+    public partial class Owner
+    {
+        [JsonPropertyName("html_url")]
+        public Uri HtmlUrl { get; set; }
+    }
+    public class Repository
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("private")]
+        public bool Private { get; set; }
+
+        [JsonPropertyName("owner")]
+        public Owner Owner { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [JsonPropertyName("updated_at")]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public Uri HtmlUrl { get; set; }
+    }
+}
